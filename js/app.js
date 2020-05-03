db3dbf727e7ef0c760afa597ebafe46388b1911d
Sylwia Fait <sylwia.fait@gmail.com>
2020-05-03 19:14:50 +0200

    Add circles

--- js/app.js
+++ js/app.js
@@ -17,8 +17,24 @@ function changeTitle() {
     })
 }
 
+function getRandomColor() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+}
+
+// another.html - circle-2
+function changeColorBorderCircles() {
+    const circles = document.querySelectorAll(".circle-2__span");
+
+    for (let i = 0; i < circles.length; i++) {
+        circles[i].style.borderColor = "#" + getRandomColor();
+        // circles[i].style.backgroundColor = "#" + getRandomColor();
+    }
+}
+
 const init = function() {
     changeTitle();
+    changeColorBorderCircles();
 };
 
 document.addEventListener("DOMContentLoaded", init);
